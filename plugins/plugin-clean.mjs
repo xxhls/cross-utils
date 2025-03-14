6d29c014c5d8210ba41464f226dff8eaa45bd2e7
heyongqi10 <heyongqi10@jd.com>
2025-03-14 22:49:35 +0800

    feat: 格式化代码

--- plugins/plugin-clean.mjs
+++ plugins/plugin-clean.mjs
@@ -1,37 +1,37 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "node:fs";
+import path from "node:path";
 
 function cleanDir(dir) {
   if (fs.existsSync(dir)) {
     const files = fs.readdirSync(dir);
-    files.forEach((file) => {
+    for (const file of files) {
       const filePath = path.join(dir, file);
       if (fs.lstatSync(filePath).isDirectory()) {
         cleanDir(filePath);
       } else {
         fs.unlinkSync(filePath);
       }
-    });
+    }
     fs.rmdirSync(dir);
   }
 }
 
 function handleOutput(output) {
   const outputs = Array.isArray(output) ? output : [output];
-  outputs.forEach(output => {
+  for (const output of outputs) {
     if (output.dir) {
       cleanDir(output.dir);
     }
-  });
+  }
 }
 
 export default function clean() {
   return {
-    name: 'clean',
+    name: "clean",
     buildStart(options) {
       if (options.output) {
         handleOutput(options.output);
       }
-    }
+    },
   };
 }
